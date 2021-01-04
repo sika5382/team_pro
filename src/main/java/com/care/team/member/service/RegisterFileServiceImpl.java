6148b49684fff5d274f968474ae6de88b0c75490
CMS <chlalthd123@gamil.com>
2021-01-04 16:48:47 +0900

    delete_dir_Delete

--- src/main/java/com/care/team/member/service/RegisterFileServiceImpl.java
+++ src/main/java/com/care/team/member/service/RegisterFileServiceImpl.java
@@ -154,7 +154,25 @@ public class RegisterFileServiceImpl implements RegisterFileService{
 
 	@Override
 	public void userDirdelete(String userId) {
-		// TODO Auto-generated method stub
+		File folder = new File(Path+"\\"+userId);
+		try {
+		    while(folder.exists()) {
+		    	
+			File[] folder_list = folder.listFiles(); //파일리스트 얻어오기
+					
+			for (int j = 0; j < folder_list.length; j++) {
+				folder_list[j].delete(); //파일 삭제 
+				System.out.println("파일이 삭제되었습니다.");		
+			}
+					
+			if(folder_list.length == 0 && folder.isDirectory()){ 
+				folder.delete(); //대상폴더 삭제
+				System.out.println("폴더가 삭제되었습니다.");
+			}
+	            }
+		 } catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
